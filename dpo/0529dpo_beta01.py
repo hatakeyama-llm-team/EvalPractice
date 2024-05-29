56fea6f492f40209c68ca537bbf81cb66c1e54d1
kanhatakeyama <none>
2024-05-29 17:53:10 +0900

    dpo

---
+++ dpo/0529dpo_beta01.py
@@ -0,0 +1,70 @@
+# %%
+import os
+from transformers import TrainingArguments
+from trl import DPOTrainer
+
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+
+# Load model and tokenizer
+model_id = "hatakeyama-llm-team/with_halcination_epoch4_ck10400"
+model = AutoModelForCausalLM.from_pretrained(model_id,device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+# %%
+
+
+train_dataset = load_dataset("hatakeyama-llm-team/dpo_data_for_tanuki", split="train")
+eval_dataset = load_dataset("hatakeyama-llm-team/dpo_data_for_tanuki", split="test")
+
+# %%
+
+
+run_name = "0529_hatakeyama_with_halci_epoch4_dpo-lr5e-6-beta0.1"
+
+os.environ["WANDB_PROJECT"] = "huggingface"
+os.environ["WANDB_NAME"] = run_name
+
+training_args = TrainingArguments(
+    output_dir= f"./{run_name}",
+    overwrite_output_dir=True,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size= 2,
+    gradient_accumulation_steps=64,
+    learning_rate=5.0e-7,
+    warmup_ratio=0.1,
+    num_train_epochs=5,
+    logging_steps=1,
+    eval_strategy="steps",
+    eval_steps=200,
+    save_strategy="epoch",
+    remove_unused_columns=False,
+    bf16=True,
+    dataloader_num_workers=24,
+    weight_decay = 0.0,
+    lr_scheduler_type="cosine",
+    gradient_checkpointing=False,
+    run_name=run_name,
+    report_to="wandb",
+    optim="adamw_torch",
+)
+
+dpo_trainer = DPOTrainer(
+    model,
+    args=training_args,
+    beta=0.1,
+    loss_type="sigmoid",
+    max_prompt_length=925,
+    max_length=1150,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    tokenizer=tokenizer,
+)
+
+# %%
+
+dpo_trainer.train()
+dpo_trainer.push_to_hub(f'hatakeyama-llm-team/{run_name}')
+
+
